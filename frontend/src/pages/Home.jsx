6cbcfcb4ae40455329992a30697fe36569eb3400
sorawiss <sorawit.bunnee@gmail.com>
2025-03-31 21:58:59 +0700

    Feat : add search bar in Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
+import { Input } from "rizzui";
+import searchIcon from '../assets/svg/search-icon.svg';
+import './Style/Home.css'
 
 
 import Post from '../components/Post';
 
 function Home() {
     return (
-        <div>
+        <div className='bg-primarylight flex flex-col items-center gap-[1rem] w-screen h-screen '>
+            <Input
+                suffix= {<img src={searchIcon} />}
+                placeholder="Search..."
+                className='search-bar w-[30rem] mt-[1.5rem]  '
+                size='lg'
+            />
+
+
             <Post />
         </div>
     );
