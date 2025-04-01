46cd0c3a7c60631cf6971136b19f321bb490c551
Peasti <sirapeet@gmail.com>
2025-04-01 21:38:05 +0700

    style: make welcome.css responsive for tablets
    Adjusted styles to support tablet screen sizes.

--- frontend/src/pages/Welcome.jsx
+++ frontend/src/pages/Welcome.jsx
@@ -3,30 +3,43 @@ import {Link} from 'react-router-dom'
 import WelcomeSVG from '../assets/svg/welcome1.svg'
 import ButtonXL from '../components/ButtonXL'
 
+import './Style/Welcome.css'
+
 function Welcome() {
 
 
     return (
-        <div className='bg-accent h-screen p-[3.5rem] pt-[3.6rem] flex flex-col gap-[4.75rem] items-center '>
+        <div className='deeped-bg-container'>
+            
+            <div className='welcome-container bg-accent h-screen p-[3.5rem] pt-[3.6rem] flex flex-col gap-[4.75rem] items-center '>
 
-            {/* Welcome-SECTION */}
-            <div className='welcome-container'>
-                <h1 className='text-primarydark'>Welcome Message</h1>
+                {/* Welcome-SECTION */}
+                <div className='text-container'>
+                    <h1 className='text-primarydark'>Welcome Message</h1>
+                    <h4 className='text-secondarydark'>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut et massa mi. Aliquam in hendrerit urna.</h4>
+                </div>
+
+
+                {/* SVG */}
+                <img src={WelcomeSVG} alt="" className='svg-container w-[20.5rem] ' />
+
+
+                {/* Input-SECTION */}
+                <div className="input-container flex flex-col items-center gap-[1rem] ">
+                    <Link to={'login'}><ButtonXL text='เข้าสู่ระบบ' /></Link>
+                    <Link to={'register'}><p className='text-[1.25rem] '>สมัครใช้งาน</p></Link>
+                </div>
+
+
             </div>
 
-
-            {/* SVG */}
-            <img src={WelcomeSVG} alt="" className='w-[20.5rem] ' />
-
-
-            {/* Input-SECTION */}
-            <div className="input-container flex flex-col items-center gap-[1rem] ">
-                <Link to={'login'}><ButtonXL text='เข้าสู่ระบบ' /></Link>
-                <Link to={'register'}><p className='text-[1.25rem] '>สมัครใช้งาน</p></Link>
+            <div className='right-container'>
+                <h2 className='right-text'>sidejob</h2>
+                {/* SVG */}
+                <img src={WelcomeSVG} alt="" className='svg-container w-[20.5rem] '/>
             </div>
-
-
         </div>
+        
     )
 }
 
