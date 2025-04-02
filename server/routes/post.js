dfa31a99bd59e9b870f8094d264d15bfba96c643
sorawiss <sorawit.bunnee@gmail.com>
2025-04-02 23:10:55 +0700

    Fix

--- server/routes/post.js
+++ server/routes/post.js
@@ -71,7 +71,7 @@ router.post('/createPost', verifyToken, async (req, res) => {
           posterID,
           salary,
           location,
-          isJob,
+          isJob : true,
           postDate: time,
           status: true
         },
