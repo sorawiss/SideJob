df3a6ef183af86bdc57a21a64e806808231077c1
sorawiss <sorawit.bunnee@gmail.com>
2025-04-02 23:10:40 +0700

    Feat : send postData to backend via NewPost.jsx

--- frontend/src/components/NewPost.jsx
+++ frontend/src/components/NewPost.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
 import { useState } from 'react';
 import { Select } from "rizzui";
+import { useContext } from 'react';
 
 import ProfileOnTop from './ProfileOnTop'
-import PriceButton from './PriceButton';
+
+import { AuthContext } from '../context/AuthContext';
 
 import arrow from '../assets/svg/arrow.svg'
 import type from '../assets/svg/type.svg'
 import picture from '../assets/svg/picture.svg'
 import location from '../assets/svg/location.svg'
+import phone from '../assets/svg/phone.svg'
 
 function NewPost({ setCreatePost }) {
 
+  const { currentUser } = useContext(AuthContext)
+
   const options = [
-    { label: 'Apple 🍎', value: 'apple' },
-    { label: 'Banana 🍌', value: 'banana' },
-    { label: 'Cherry 🍒', value: 'cherry' },
+    { label: 'บันเทิง', value: '1' },
+    { label: 'การเรียน', value: '2' },
+    { label: 'ทำความสะอาด', value: '3' },
   ]
 
   const [value, setValue] = useState(null);
@@ -23,10 +28,61 @@ function NewPost({ setCreatePost }) {
   const [formData, setFormData] = useState(
     {
       title: '',
-
+      salary: '',
+      details: '',
+      categoryID: null
     }
   )
 
+  console.log(currentUser)
+
+  const handleSelectChange = (select) => {
+    setValue(select);
+    setFormData((prev) => ({
+      ...prev,
+      categoryID: select.value,
+    }));
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: name == 'categoryID' ? (value) : value
+    }));
+  };
+
+
+
+  // API Call
+  async function submitHandle(e) {
+    e.preventDefault()
+    const postData = {
+      title: formData.title,
+      salary: formData.salary,
+      details: formData.details,
+      categoryID : formData.categoryID,
+      posterID : currentUser.id
+    }
+
+    try {
+      fetch('http://localhost:3333/createPost', {
+        method : 'POST',
+        credentials: 'include',
+        headers: {
+          'content-type': 'application/json'
+        },
+        body: JSON.stringify(postData)
+      })
+
+      console.log("add post to database success" + postData)
+      setCreatePost()
+    }
+    catch (error) {
+      console.log(error)
+    }
+
+  }
 
 
   return (
@@ -42,17 +98,20 @@ function NewPost({ setCreatePost }) {
         <div className="main-post-section-wrapper flex flex-col gap-[1rem] ">
           <ProfileOnTop />
 
-          <textarea className='title-text h-[4.5rem] outline-none ' placeholder='หัวเรื่อง..' onInput={(e) => {
+          <textarea className='title-text h-[4.5rem] outline-none ' placeholder='หัวเรื่อง..' name='title' onInput={(e) => {
             e.target.style.height = 'auto';
             e.target.style.height = `${e.target.scrollHeight}px`;
-          }} />
+          }}
+            onChange={handleChange} />
 
           <div className="detail-wrapper w-[100%] flex flex-col gap-[1rem] items-center ">
             <div className="detail-input w-[100%] " >
               <textarea placeholder='รายละเอียด(ไม่บังคับ)...' className='bg-primarylight w-[100%] min-h-[12rem] rounded-[16px] p-[1rem] outline-none ' onInput={(e) => {
                 e.target.style.height = 'auto';
                 e.target.style.height = `${e.target.scrollHeight}px`
-              }} />
+              }}
+                name='details'
+                onChange={handleChange} />
             </div>
 
             <div className="add-more-detail flex gap-[2.5rem] ">
@@ -62,7 +121,8 @@ function NewPost({ setCreatePost }) {
                   placeholder="ประเภท"
                   options={options}
                   value={value}
-                  onChange={setValue}
+                  name='categoryID'
+                  onChange={handleSelectChange}
                   selectClassName="ring-0 focus:ring-0 border-none "
                 />
               </div>
@@ -78,9 +138,22 @@ function NewPost({ setCreatePost }) {
           </div>
         </div>
 
-        <div className="price-set bg-primarydark rounded-[16px] px-[1rem] py-[4px] mt-[0.9rem] ">
-          <input type="number" placeholder='ราคา..' className='price-text outline-none ' />
+        <div className="contact-wrapper">
+          <p className='text-backgrounddark ' >การติดต่อ(ไม่บังคับ)</p>
+
+          <div className="contact-input">
+            <div className="more-detail-wrapper">
+              <img src={phone} alt="" />
+              <p className='p2 ' >{currentUser.phone_number}</p>
+            </div>
+          </div>
         </div>
+
+        <div className="price-set bg-primarydark rounded-[16px] px-[1rem] py-[4px] mt-[0.9rem] ">
+          <input type="number" placeholder='ราคา..' className='price-text outline-none ' onChange={handleChange} name='salary'  />
+        </div>
+
+        <button onClick={submitHandle} >Post</button>
       </div>
     </div>
   )
