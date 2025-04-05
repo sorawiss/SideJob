c876654e35a339918618c8802bdf382ac4710521
sorawiss <sorawit.bunnee@gmail.com>
2025-04-05 17:51:27 +0700

    Feat : add nav

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
-export default function Nav( { setCreatePost } ) {
+import plus from '../assets/svg/plus.svg'
+
+export default function Nav({ setCreatePost }) {
   return (
 
 
     <div className='nav-container fixed bottom-0 w-screen h-[3rem] bg-primarydark ' >
-        <h2 className='text-primarylight cursor-pointer inline ' onClick={setCreatePost} >Post</h2>
+      <div className="nav-items-container flex gap-[3rem] justify-center items-center ">
+        <h2 className='text-primarylight cursor-pointer ' >จ้างงาน</h2>
+        <img src={plus} alt="" className='cursor-pointer'  onClick={setCreatePost} />
+        <h2 className='text-primarylight cursor-pointer' >หางาน</h2>
+      </div>
+
     </div>
   )
 }
--- server/routes/post.js
+++ server/routes/post.js
@@ -71,7 +71,7 @@ router.post('/createPost', verifyToken, async (req, res) => {
           posterID,
           salary,
           location,
-          isJob : true,
+          isJob,
           postDate: time,
           status: true
         },
