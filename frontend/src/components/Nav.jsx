691d5f0e3f178c44df8bb75a53181ab313e26d79
sorawiss <sorawit.bunnee@gmail.com>
2025-04-05 22:34:39 +0700

    Style : nav buttom bar

--- frontend/src/components/Nav.jsx
+++ frontend/src/components/Nav.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
 
 import plus from '../assets/svg/plus.svg'
+import findWork from '../assets/svg/nav/findWork.svg'
+import hire from '../assets/svg/nav/hire.svg'
+import notification from '../assets/svg/nav/notification.svg'
+import profile from '../assets/svg/nav/profile.svg'
+
+
+
 
 export default function Nav({ setCreatePost, setPage }) {
   return (
 
 
-    <div className='nav-container fixed bottom-0 w-screen h-[3rem] bg-primarydark ' >
+    <div className='nav-container fixed bottom-0 w-screen h-[5rem] bg-primarydark ' >
       <div className="nav-items-container flex gap-[3rem] justify-center items-center ">
-        <h2 className='text-primarylight cursor-pointer' onClick={() => { setPage(true) }} >หางาน</h2>
+        <img src={findWork} alt="" className='cursor-pointer' onClick={() => setPage(true)} />
+        <img src={hire} alt="" className='cursor-pointer' onClick={() => setPage(false)} />
         <img src={plus} alt="" className='cursor-pointer' onClick={setCreatePost} />
-        <h2 className='text-primarylight cursor-pointer ' onClick={() => { setPage(false) }} >จ้างงาน</h2>
+        <img src={notification} alt="" className='cursor-pointer' />
+        <img src={profile} alt="" className='cursor-pointer'/>
       </div>
 
     </div>
