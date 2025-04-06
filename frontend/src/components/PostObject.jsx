06173b652594db58c2f531f0af5a7a0b38b8acc9
sorawiss <sorawit.bunnee@gmail.com>
2025-04-06 15:34:13 +0700

    Refactor :

--- frontend/src/components/PostObject.jsx
+++ frontend/src/components/PostObject.jsx
@@ -34,7 +34,7 @@ function PostObject({ postDate, title, detail, price, fname, lname, category, lo
             <div className="profile flex gap-[0.5rem] ">
                 {/* ProfilePic */}
                 <Link to={'profile/' + posterID} className=''>
-                    <img src={ profilePic ? profilePic : profile } alt="" className='w-[2.6rem] h-[2.6rem] ' />
+                    <img src={ profilePic ? "/upload/" + profilePic : profile } alt="" className='w-[2.6rem] h-[2.6rem] rounded-full object-cover ' />
                 </Link>
     
 
