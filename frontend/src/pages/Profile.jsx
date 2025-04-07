8e67e06e103ad298135ceb1d25e9bada82f7c532
sorawiss <sorawit.bunnee@gmail.com>
2025-04-07 20:44:05 +0700

    style : add pencil and arrow at profile.jsx

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import { useQuery } from '@tanstack/react-query'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 import profilePlaceHolder from '../assets/svg/profile_placeholder.svg'
 import phone from '../assets/svg/phone.svg'
-import pen from '../assets/svg/pen.svg'
 import mail from '../assets/svg/gmail.svg'
 import linepic from '../assets/svg/line.svg'
 
@@ -34,11 +33,15 @@ function findAvg(data) {
 
 function Profile() {
 
-  const { id } = useParams()
-
+  // Navigate back
+  const navigate = useNavigate();
+  const handleGoBack = () => {
+    navigate(-1); 
+  };
 
 
   // Get Profile
+  const { id } = useParams()
   const { isPending, error, data } = useQuery({
     queryKey: ['inPost', id],
     queryFn: () =>
@@ -56,10 +59,21 @@ function Profile() {
 
 
   return (
-    <div className="home-container bg-primarylight flex flex-col items-center gap-[1rem] w-screen min-h-screen " >
-      <div className="top-wrapper">
+    <div className="home-container bg-primarylight flex flex-col items-center gap-[3rem] w-screen min-h-screen py-[4rem] " >
+
+      <div className="top-wrapper w-[28rem] flex flex-col items-center gap-[2rem]  ">
+        <div className="menu-wrapper w-full flex justify-between items-center ">
+          <svg className='arrow size-[2rem] cursor-pointer' viewBox="0 0 49 30" fill="none" xmlns="http://www.w3.org/2000/svg" onClick={handleGoBack}>
+            <path d="M47 15L2 15M2 15L17.5 28M2 15L17.5 2" stroke="#1B1B1B" strokeWidth="4" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+
+          <svg className='pencil stroke-primarydark size-[2rem] ' viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
+            <path d="M44.393 12.8342L7.07371 50.1483C5.96411 51.2575 5.24209 52.6952 5.01499 54.2477L3 68L16.7552 65.9849C18.308 65.7568 19.7457 65.0334 20.8544 63.9224L58.1664 26.6083M44.393 12.8342L52.5149 4.70813C53.0563 4.1666 53.6991 3.73702 54.4065 3.44394C55.1139 3.15085 55.8721 3 56.6378 3C57.4036 3 58.1618 3.15085 58.8692 3.44394C59.5766 3.73702 60.2194 4.1666 60.7607 4.70813L66.292 10.2397C66.8335 10.7811 67.263 11.4239 67.5561 12.1313C67.8492 12.8388 68 13.597 68 14.3628C68 15.1286 67.8492 15.8868 67.5561 16.5943C67.263 17.3017 66.8335 17.9445 66.292 18.4859L58.1664 26.6083M44.393 12.8342L58.1664 26.6083" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" />
+          </svg>
+
+        </div>
         <div className="profile-desc-wrapper flex flex-col items-center gap-[2rem] ">
-          <div className="profile-wrapper  " style={{ marginTop: '6rem' }}>
+          <div className="profile-wrapper  " >
             <img className='w-[13rem] h-[13rem] rounded-full object-cover object-center ' src={data.profile_picture ? "/upload/" + data.profile_picture : profilePlaceHolder} alt="" />
           </div>
 
@@ -84,7 +98,7 @@ function Profile() {
         </div>
       </div>
 
-      
+
       <ShowRating avgRating={resultOfAvgRating.avg} numberOfRating={resultOfAvgRating.count} />
 
       <div className='post-grid-container bg-primarylight flex flex-col items-center gap-[1rem]  '>
