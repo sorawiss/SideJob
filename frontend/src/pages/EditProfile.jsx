79da4fa12acad4ee4c07c45b18e52060b0169508
sorawiss <sorawit.bunnee@gmail.com>
2025-04-08 21:22:04 +0700

    Feat :  edit profile page

--- frontend/src/pages/EditProfile.jsx
+++ frontend/src/pages/EditProfile.jsx
@@ -6,6 +6,10 @@ import Loading from '../components/Loading';
 import profilePlaceHolder from '../assets/svg/profile_placeholder.svg'
 
 import { Input } from "rizzui";
+import { Button } from "rizzui";
+
+import Arrow from '../components/Arrow';
+
 
 
 
@@ -25,12 +29,16 @@ function EditProfile() {
 
 
   return (
-    <div className="edit-profile-container flex flex-col items-center py-[1.5rem] ">
+    <div className="edit-profile-container flex flex-col items-center content-center py-[4rem] bg-primarylight min-h-screen px-[3rem] w-screen gap-[5rem] ">
+      <div className="menu-wrapper w-[28rem] flex justify-between items-center ">
+        <Arrow />
+      </div>
+
       <div className="profile-wrapper  " >
         <img className='w-[13rem] h-[13rem] rounded-full object-cover object-center ' src={data.profile_picture ? "/upload/" + data.profile_picture : profilePlaceHolder} alt="" />
       </div>
 
-      <form action="">
+      <form className='edit-profile-form w-[18rem] flex flex-col gap-[1rem] items-center ' action="">
         <Input
           label="ชื่อ"
           placeholder={data.fname}
@@ -50,8 +58,10 @@ function EditProfile() {
         <Input
           label="คำอธิบาย"
           placeholder={data.detail}
-        />
+        />  
       </form>
+
+      <Button className='bg-primarydark text-white rounded-[16px] ' >Button</Button>
 
 
     </div>
