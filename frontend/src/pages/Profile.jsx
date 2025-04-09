b5b2a40de1a0547669e867f0c98dae7bb4a1864f
sorawiss <sorawit.bunnee@gmail.com>
2025-04-09 21:25:25 +0700

    Fix : hide edit botton from other user

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { useQuery } from '@tanstack/react-query'
 import { useParams, Link } from 'react-router-dom'
+import { useContext } from 'react'
+import { AuthContext } from '../context/AuthContext'
 
 import profilePlaceHolder from '../assets/svg/profile_placeholder.svg'
 import phone from '../assets/svg/phone.svg'
@@ -38,6 +40,7 @@ function Profile() {
 
   // Get Profile
   const { id } = useParams()
+  const { currentUser } = useContext(AuthContext)
   const { isPending, error, data } = useQuery({
     queryKey: ['profile', id],
     queryFn: () =>
@@ -59,11 +62,14 @@ function Profile() {
         <div className="menu-wrapper w-full flex justify-between items-center ">
           <Arrow />
 
-          <Link to={'/editProfile/' + id} >
+          {currentUser.id === data.id ? (
+            <Link to={'/editProfile/' + id} >
             <svg className='pencil stroke-primarydark size-[2rem] ' viewBox="0 0 71 71" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M44.393 12.8342L7.07371 50.1483C5.96411 51.2575 5.24209 52.6952 5.01499 54.2477L3 68L16.7552 65.9849C18.308 65.7568 19.7457 65.0334 20.8544 63.9224L58.1664 26.6083M44.393 12.8342L52.5149 4.70813C53.0563 4.1666 53.6991 3.73702 54.4065 3.44394C55.1139 3.15085 55.8721 3 56.6378 3C57.4036 3 58.1618 3.15085 58.8692 3.44394C59.5766 3.73702 60.2194 4.1666 60.7607 4.70813L66.292 10.2397C66.8335 10.7811 67.263 11.4239 67.5561 12.1313C67.8492 12.8388 68 13.597 68 14.3628C68 15.1286 67.8492 15.8868 67.5561 16.5943C67.263 17.3017 66.8335 17.9445 66.292 18.4859L58.1664 26.6083M44.393 12.8342L58.1664 26.6083" strokeWidth="6" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
           </Link>
+          ) : null}
+          
 
         </div>
         <div className="profile-desc-wrapper flex flex-col items-center gap-[2rem] ">
