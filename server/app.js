882b544a6a24bce38b4db042050cbba8dbccdfe9
sorawiss <sorawit.bunnee@gmail.com>
2025-04-11 21:37:59 +0700

    Fix : origin to render

--- server/app.js
+++ server/app.js
@@ -15,8 +15,9 @@ app.use(cookieParser())
 
 
 // Cors
+const allowedOrigins = process.env.ORIGIN || "http://localhost:5173"
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: allowedOrigins,
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
