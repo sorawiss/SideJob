426f8845d0785d5aa43d31fdec8620afc2b7b9cd
sorawiss <sorawit.bunnee@gmail.com>
2025-04-15 15:47:54 +0700

    Fix : divide bin and profile

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -21,9 +21,7 @@ function Review({ member, detail, date, rating, reviewId }) {
 
     return (
         <div className='review-container bg-primarylight w-[100%] rounded-[16px] py-[0.5rem] px-[1rem] flex flex-col gap-[1rem] '>
-            <Link to={'/profile/' + member.id}>
-                <ProfileOnTop fname={member.fname} lname={member.lname} date={date} reviewerID={member.id} reviewId={reviewId} profilePic={member.profile_picture} />
-            </Link>
+            <ProfileOnTop fname={member.fname} lname={member.lname} date={date} reviewerID={member.id} reviewId={reviewId} profilePic={member.profile_picture} />
 
             <div className="star-wrapper">
                 <Rating value={rating} readonly ratedColor="amber" />
