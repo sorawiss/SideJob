c8ae97553631e8b463a066e53fbff4168b21233e
sorawiss <sorawit.bunnee@gmail.com>
2025-04-17 15:40:06 +0700

    Refactor : use PATCH instead of PUT

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const allowedOrigins = process.env.ORIGIN || "http://localhost:5173"
 const corsOptions = {
   origin: allowedOrigins,
   credentials: true,
-  methods: ["GET", "POST", "PUT", "DELETE"],
+  methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
   allowedHeaders: ["Content-Type", "Authorization"],
 };
 
